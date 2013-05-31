399abd3240f42eac958a35cf6705f5b87ec92ef6
ywilkof <ywilkof@gmail.com>
2013-05-31 14:42:45 +0300

    new filesToLoad

--- PuzzleCode/src/gui/HintPopupMenu.java
+++ PuzzleCode/src/gui/HintPopupMenu.java
@@ -46,86 +46,60 @@ final class HintPopupMenu extends JPopupMenu {
 
 		Arrays.sort(hintArray); // sort before replace with entity name
 
-		// add the hints to the popup menu
-		for (int i=0; i<hintArray.length; i++){ // sorted by predicate
-			add(new HintPopupSubmenu("Hint #" + (i+1), hintArray[i].getHintText())); 
+		//		// add the hints to the popup menu
+		//		for (int i=0; i<hintArray.length; i++){ // sorted by predicate
+		//			add(new HintPopupSubmenu("Hint #" + (i+1), hintArray[i].getHintText())); 
+		//		}
+
+		for (int i=0; i<hintArray.length; i++) {
+			add(new HintItem(hintArray[i].getHintText()));
 		}
 	}
 
-	/**
-	 * An entry in the popoup that leads to the HintItem. 
-	 * @author yonatan
-	 *
-	 */
-	private class HintPopupSubmenu extends JMenu {
-		private String popupText;
-		private HintItem hintItem;		
-		private HintPopupSubmenu(String popupText, String hintText) {
+	private class HintItem extends JPanel {
+		private final String hintText;
+		private JLabel lbl;
+		private HintItem(String hintText) {
+
 			super();
-			this.setIcon(new ImageIcon(CrosswordView.class.getResource("/resources/tip.png")));
-			this.popupText = popupText;
-			hintItem = createHintItem(hintText);
-			add(hintItem);
+
+			this.hintText = hintText; // save the hint text for later
+			this.setLayout(new BorderLayout());
+			lbl = new JLabel("<html><p><left>&nbsp;&nbsp;&nbsp;&nbsp;" + createEchoString(hintText.length()) + "</p></html>");
+			add(lbl, BorderLayout.CENTER);
+			JButton imageBtn = new JButton(new ImageIcon(HintPopupMenu.class.getResource("/resources/locked.png")));
+
+			// set button behavior
+			imageBtn.addActionListener(new LockButtonListener(this.lbl));
+
+			add(imageBtn, BorderLayout.EAST);
 		}
 
-		private HintItem createHintItem(String hintText) {
-			return new HintItem(hintText);
-		}
-		public String getText() {
-			return popupText;
-		}
-
-		/**
-		 * The hint box: hint text and lock/unlock button
-		 * @author yonatan
-		 *
-		 */
-		private class HintItem extends JPanel {
-			private final String hintText;
-			private JLabel lbl;
-			private HintItem(String hintText) {
-
-				super();
-
-				this.hintText = hintText; // save the hint text for later
-				this.setLayout(new BorderLayout());
-				lbl = new JLabel("<html><p><left>&nbsp;&nbsp;&nbsp;&nbsp;" + createEchoString(hintText.length()) + "</p></html>");
-				add(lbl, BorderLayout.CENTER);
-				JButton imageBtn = new JButton(new ImageIcon(HintPopupMenu.class.getResource("/resources/locked.png")));
-
-				// set button behavior
-				imageBtn.addActionListener(new LockButtonListener(this.lbl));
-
-				add(imageBtn, BorderLayout.EAST);
+		private String createEchoString(int length) {
+			StringBuilder str = new StringBuilder();
+			for (int i = 0; i<length; i++ ) {
+				str.append('*');
 			}
-			
-			private String createEchoString(int length) {
-				StringBuilder str = new StringBuilder();
-				for (int i = 0; i<length; i++ ) {
-					str.append('*');
-				}
-				return str.toString();
+			return str.toString();
+		}
+
+		private class LockButtonListener implements ActionListener {
+			boolean isLocked = true;
+			JLabel lbl;
+			private LockButtonListener(JLabel lbl) {
+				this.lbl = lbl;
 			}
-			
-			private class LockButtonListener implements ActionListener {
-				boolean isLocked = true;
-				JLabel lbl;
-				private LockButtonListener(JLabel lbl) {
-					this.lbl = lbl;
-				}
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					if (isLocked) {
-						JButton btn = (JButton) e.getSource();
-						btn.setIcon(new ImageIcon(HintPopupMenu.class.getResource("/resources/unlocked.png")));
-						btn.setEnabled(false);
-						HintPopupMenu.this.usedHintsCounter++; // increment the hints used in this popup
-						this.lbl.setText("<html><p><left>&nbsp;&nbsp;&nbsp;&nbsp;" + HintItem.this.hintText);
-						
-					}
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (isLocked) {
+					JButton btn = (JButton) e.getSource();
+					btn.setIcon(new ImageIcon(HintPopupMenu.class.getResource("/resources/unlocked.png")));
+					btn.setEnabled(false);
+					HintPopupMenu.this.usedHintsCounter++; // increment the hints used in this popup
+					this.lbl.setText("<html><p><left>&nbsp;&nbsp;&nbsp;&nbsp;" + HintItem.this.hintText);
+
 				}
 			}
 		}
 	}
-
 }
