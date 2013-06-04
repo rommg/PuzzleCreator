21005c09b40e897b4b6051362df8bca7b196f11b
Guy Romm <guyr@wix.com>
2013-06-04 17:09:19 +0300

    db debug

--- PuzzleCode/src/utils/GuiDBConnector.java
+++ PuzzleCode/src/utils/GuiDBConnector.java
@@ -5,12 +5,7 @@ import java.util.List;
 
 public interface GuiDBConnector {
 	
-	/**
-	 * This method returns a list of hints for the given entityId
-	 * @param entityId
-	 * @return
-	 */
-	public List<String> getHints(String entityId);
+
 	
 	/**
 	 * Pull a question and an answer from the DB
