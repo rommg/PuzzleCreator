f353772a2a2ff5b09d25bf7e60f7be5a9481684c
ywilkof <ywilkof@gmail.com>
2013-06-11 14:15:27 +0300

    added solve for waitview

---
+++ PuzzleCode/src/gui/InputSquare.java
@@ -0,0 +1,36 @@
+package gui;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+
+/**
+ * A regular Square (with no definition) in the crossword
+ * @author yonatan
+ *
+ */
+final class InputSquare extends AbstractSquarePanel{
+	
+	private SquareTextField field;
+
+	public SquareTextField getField() {
+		return field;
+	}
+
+	public InputSquare(SquareTextField field, int row, int col) {
+		super(row, col);
+		super.labelCount = 1;
+		setLayout(new BorderLayout());
+		this.field = field;
+		setBackground(Color.WHITE);		
+		field.setBackground(super.getBackground());
+		add(field, BorderLayout.CENTER);
+	}
+
+//	@Override
+//	protected float getFontSize(JDefinitionLabel lbl) {
+//		// TODO Auto-generated method stub
+//		return 0;
+//	}
+
+
+}
