c0b07bb133e35a01e16eb6a5639e00f69c2b71a0
ywilkof <ywilkof@gmail.com>
2013-06-17 23:40:19 +0300

    new screen on logon

---
+++ PuzzleCode/src/ui/CredentialsView.java
@@ -0,0 +1,77 @@
+package ui;
+
+import javax.swing.BoxLayout;
+import javax.swing.JFormattedTextField;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+
+import core.PuzzleCreator;
+
+@SuppressWarnings("serial")
+public class CredentialsView extends JPanel {
+
+	private JTextField dbServerAddressField;
+	private JFormattedTextField dbServerPortField;
+	private JTextField username;
+	private LimitedTextField password;
+
+
+
+	/**
+	 * Create the panel.
+	 */
+	public CredentialsView() {
+		
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		dbServerAddressField = new LimitedTextField(25);
+		dbServerAddressField.setText(PuzzleCreator.dbServerAddress);
+		dbServerAddressField.selectAll();
+
+		dbServerPortField =  new JFormattedTextField("####");
+		dbServerPortField.setText(PuzzleCreator.dbServerPort);
+		dbServerPortField.selectAll();
+
+		username = new LimitedTextField(15);
+		username.setText(PuzzleCreator.username);
+		username.requestFocusInWindow();
+		username.selectAll();
+
+		password = new LimitedTextField(10);
+
+		add(new JLabel("DB Server Address:"));
+		add(dbServerAddressField);
+		add(new JLabel("DB Server Port:"));
+		add(dbServerPortField);
+		add(new JLabel("Username:"));
+		add(username);
+		add(new JLabel("Password:"));
+		add(password);
+		
+	}
+
+
+
+	String getDbServerAddressField() {
+		return dbServerAddressField.getText();
+	}
+
+
+
+	String getDbServerPortField() {
+		return dbServerPortField.getText();
+	}
+
+
+
+	String getUsername() {
+		return username.getText();
+	}
+
+
+
+	String getPassword() {
+		return password.getText();
+	}
+}
