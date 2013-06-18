f6321aab14370e18f356c522ce85f5f7198c22b8
Guy Romm <guyr@wix.com>
2013-06-18 17:00:47 +0300

    templates now in resources

--- PuzzleCode/src/core/algorithm/AlgorithmWorker.java
+++ PuzzleCode/src/core/algorithm/AlgorithmWorker.java
@@ -504,8 +504,8 @@ public class AlgorithmWorker extends SwingWorker<BoardSolution, String> {
 		this.definitions.clear();
 		this.usedEntities.clear();
 		String fileName = "" + size + "x" + size + "_" + templateNum + ".tmp";
-		File templateFile = new File(PuzzleCreator.appDir + "templates",
-				fileName);
+		File templateFile = new File(getClass().getClassLoader().getResource("resources/"+ fileName).getFile());
+		//File templateFile = new File(PuzzleCreator.appDir + "templates",fileName);
 
 		FileReader in = new FileReader(templateFile);
 		BufferedReader bin = new BufferedReader(in);
