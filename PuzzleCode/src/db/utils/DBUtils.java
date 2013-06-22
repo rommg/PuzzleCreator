6ebe60b6515b577a5d9b97cbbd49c65feae90d22
Guy <rommguy@gmail.com>
2013-06-22 23:36:04 +0300

    final version for submission

--- PuzzleCode/src/db/utils/DBUtils.java
+++ PuzzleCode/src/db/utils/DBUtils.java
@@ -6,8 +6,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import core.algorithm.AlgorithmWorker;
 import core.algorithm.Answer;
 import core.algorithm.PuzzleDefinition;
 import db.DBConnection;
